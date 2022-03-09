c2cab2860a640474d46ba5ebf216ae39f74a5d11
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-03-09 05:35:27 -0800

    Add files via upload

---
+++ mat/mat-a-main/mat.cpp
@@ -0,0 +1,13 @@
+#include<stdexcept>
+#include<string>
+#include<iostream>
+#include "mat.hpp"
+
+using namespace std ;
+
+namespace ariel{
+        string mat (int r ,int c,char n,char m){
+        string tmp = "elad";
+        return tmp;
+    }
+}
