48179e69200030541a3984b83ada5476b05222ef
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-03-13 16:20:17 +0200

    need to know the ascii range

--- mat/mat-b-main/mat.cpp
+++ mat/mat-b-main/mat.cpp
@@ -7,12 +7,26 @@
 using namespace std ;
 
 namespace ariel{
+
         string mat (int r ,int c,char n,char m){
-            string result = "";
+            string result = " ";
+            int min;
+            int max;
 
             //check invaild input
             if (r%2==0||c%2==0){
                 throw "invaild input - the input should be odd number";
+            }
+            if (r == 0 || c ==0 ){
+                throw "invaild input - the input should be bigger from zero ";
+            }
+            // The two input tests can be merged, but I did not merge because of Dvir's test file.
+            if (r < 0 || c < 0 ){
+                throw "invaild input - the input should be positive ";
+            }
+            // if for invaild char from ascii table 
+            if ((n < min || n> max)||(m < min || m > max)){
+                throw "invaild input - the symbols should in the range";
             }
             // in case r=c=1
             if (r==1){
@@ -21,9 +35,10 @@ namespace ariel{
             }
             // regular cases
             vector <string> v1 ;
-            string tmpMatrix [r][c];
+            char tmpMatrix [r][c];
             int x = 0;
             int y = 0;
+            while (x < r && y <c ){
             for (int i = 0; i < r; i++){
                 for (int j = 0; j < c; j++){
                // fill the first and last line in the first char.
@@ -32,13 +47,17 @@ namespace ariel{
                // fill the first and last colum in  the first char.
                tmpMatrix[0][j] = n;
                tmpMatrix[c-1][j] = n;
-
-
-
-
-
+               // ...
 
                 }  
+                }
+            }
+
+            // print the matrix
+            for (int i =0 ; i<r; i++){
+                for (int j=0 ;j <c ;j++){
+                    cout<< result <<tmpMatrix[i][j];
+                }
             }
           
         
