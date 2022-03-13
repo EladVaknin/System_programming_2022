9a794ef612d980cc32d054f040030158a601fffe
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-03-14 01:42:11 +0200

    continue implem

--- mat/mat-b-main/mat.cpp
+++ mat/mat-b-main/mat.cpp
@@ -8,10 +8,10 @@ using namespace std ;
 
 namespace ariel{
 
-        string mat (int r ,int c,char n,char m){
+        string mat (int r ,int c,char n1,char m2){
             string result = " ";
-            int min;
-            int max;
+            int min = 33;
+            int max = 127;
 
             //check invaild input
             if (r%2==0||c%2==0){
@@ -25,12 +25,12 @@ namespace ariel{
                 throw "invaild input - the input should be positive ";
             }
             // if for invaild char from ascii table 
-            if ((n < min || n> max)||(m < min || m > max)){
+            if ((n1 < min || n1> max)||(m2 < min || m2 > max)){
                 throw "invaild input - the symbols should in the range";
             }
             // in case r=c=1
-            if (r==1){
-                result = n;
+            if (r == 1){
+                result = n1;
                 return result;
             }
             // regular cases
@@ -38,15 +38,15 @@ namespace ariel{
             char tmpMatrix [r][c];
             int x = 0;
             int y = 0;
-            while (x < r && y <c ){
+            while (x < r && y < c ){
             for (int i = 0; i < r; i++){
                 for (int j = 0; j < c; j++){
                // fill the first and last line in the first char.
-               tmpMatrix[i][0] = n;
-               tmpMatrix[i][c-1]=n;
+               tmpMatrix[i][0] = n1;
+               tmpMatrix[i][c-1] = n1;
                // fill the first and last colum in  the first char.
-               tmpMatrix[0][j] = n;
-               tmpMatrix[c-1][j] = n;
+               tmpMatrix[0][j] = n1;
+               tmpMatrix[c-1][j] = n1;
                // ...
 
                 }  
