70b408021f7266e69a77274d34afe1605f493789
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-03-20 16:30:07 +0200

    start to imple

---
+++ notebook/notebook-a-main/Test.cpp
@@ -0,0 +1,23 @@
+/**
+ * AUTHORS: <Elad Vaknin>
+ * 
+ * Date: 2022-03
+ */
+
+
+#include "Notebook.hpp"
+#include "Direction.hpp"
+#include "doctest.h"
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+
+using namespace std;
+using namespace ariel;
+using ariel::Direction;
+
+
+TEST_CASE("Notebook working") {
+   Notebook notebook;
+   //notebook.write();
+}
