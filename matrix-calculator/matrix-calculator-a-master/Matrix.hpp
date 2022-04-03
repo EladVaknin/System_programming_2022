27a140dac5d24f04a0652573f697bc2cfb84f329
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-04-03 14:11:07 +0300

    first upload - demo not working yet

---
+++ matrix-calculator/matrix-calculator-a-master/Matrix.hpp
@@ -0,0 +1,52 @@
+/**
+ * AUTHORS: <Elad Vaknin>
+ * 
+ * Date: 2022-03
+ */
+#include<vector>
+#include <map>
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+using namespace std;
+#pragma once
+
+namespace zich {
+     class Matrix{
+
+     private:
+     
+     vector <double> tmpMatrix;
+     int row;
+     int colum;
+
+    public: 
+        Matrix (vector<double>,int r,int c);  // constructor
+        Matrix operator+ () const; 
+        Matrix & operator++ ();
+        Matrix operator++ (int);
+        Matrix operator+ (Matrix &other_num);
+        Matrix operator- (Matrix &other_num);
+        Matrix & operator-- ();
+        Matrix operator-- (int);
+        Matrix operator- () const;
+        Matrix& operator+= (const Matrix &num);
+        Matrix& operator-= (const Matrix &num);
+        // *
+        Matrix operator* (double factor);
+        friend Matrix operator* (double factor, const Matrix);
+        Matrix& operator *= (const Matrix &num);
+
+        // < > = 
+        bool operator> (const Matrix &other_num) const;
+        bool operator< (const Matrix &other_num) const;
+        bool operator== (const Matrix &other_num) const;
+        bool operator<= (const Matrix &other_num) const;
+        bool operator>= (const Matrix &other_num) const;
+        bool operator!= (const  Matrix &other_num) const;
+        // << >> 
+        friend ostream& operator<< (ostream& stream, const Matrix& num);
+        friend istream& operator>> (istream& stream, const Matrix& num);
+        };
+}
